104f56f2b5c01e5218a1650b11f1ce2d8a25f22a
marcos <marcos.noriyuki.miyata@gmail.com>
2023-12-22 15:16:32 -0300

    Add name input and form submission functionality

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,16 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
   methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
-    }
-  }
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
